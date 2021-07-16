17a89a6834428e3baadecfdb87bc4ae9572c4ed0
Jackseed <vinkho.c@gmail.com>
2021-07-16 11:00:36 +0200

    play pause on click in background

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,24 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
+  isPlaying = false;
   background = '--img: url(../../assets/start.gif)';
+  startTime = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  play() {
-    this.background = '--img: url(../../assets/play.gif)';
-
+  async play() {
+    const delta = Date.now() - this.startTime;
+    if (delta < 3200) return;
+    // if not playing, play; otherwise pause
+    if (!this.isPlaying) {
+      this.background = '--img: url(../../assets/play.gif)';
+      this.isPlaying = true;
+    } else {
+      this.background = '--img: url(../../assets/pause.gif)';
+      this.isPlaying = false;
+    }
+    this.startTime = Date.now();
     setTimeout(
-      () => (this.background = '--img: url(../../assets/playing.gif)'),
-      3200
-    );
-  }
-
-  pause() {
-    this.background = '--img: url(../../assets/pause.gif)';
-    setTimeout(
-      () => (this.background = '--img: url(../../assets/start.gif)'),
+      () =>
+        this.isPlaying
+          ? (this.background = '--img: url(../../assets/playing.gif)')
+          : (this.background = '--img: url(../../assets/start.gif)'),
       3200
     );
   }
