dabc664cf018333d360b739316b7628d371ac42c
Jackseed <vinkho.c@gmail.com>
2021-08-20 14:48:01 +0200

    pubsub delete & nova scrapping copy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const puppeteer = require('puppeteer');
 const functions = require('firebase-functions');
 const axios = require('axios').default;
 const axiosRetry = require('axios-retry');
@@ -11,8 +12,9 @@ admin.initializeApp();
 const nova = [
   {
     novaChannel: 'Radio Nova',
-    playlistId: '6GhFJfQ9pTY578m3l3CfOd',
-  },/*
+    playlistId: '6wQtOcO0IvsscZM1TBFAlO',
+    frequence: '910',
+  } /*
   {
     novaChannel: 'Nouvo nova',
     playlistId: '22x26Yktyw49iHcC5l0GOR',
@@ -28,17 +30,53 @@ const nova = [
   {
     novaChannel: 'Nova danse',
     playlistId: '24WXdbI5caT1TZKlipmxzE',
-  }, */
+  }, */,
 ];
+const limitTracksToCheck = 10;
 
-/// PUB SUB JOB
+/// PUB SUB: SCRAPE NOVA & ADD TO SPOTIFY EVERY 5 MIN
+export const saveNovaEveryFiveMinutes = functions.pubsub
+  .schedule('every 5 minutes')
+  .onRun(async (req: any, res: any) => {
+    // create a request per radio
+    const requestsArray = nova.map((radio) => {
+      const request = axios({
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        url: 'https://us-central1-nova-jukebox.cloudfunctions.net/saveNovaOnSpotify',
+        data: {
+          playlistId: radio.playlistId,
+          frequence: radio.frequence,
+        },
+        method: 'POST',
+      });
+
+      return request;
+    });
+    // send requests
+    await Promise.all(
+      requestsArray.map(async (request) => {
+        return await request
+          .then((response: any) => {
+            console.log('promises well sent');
+          })
+          .catch((err: any) => console.log('Something broke!', err));
+      })
+    )
+      .then(() => {
+        console.log('All good!');
+      })
+      .catch((err) => console.log('something went wrong.. ', err));
+  });
+
+/// PUB SUB: SAVE & DELETE EVERY WEEK
 export const saveAndDeleteNovaEveryWeek = functions
   .runWith({
     timeoutSeconds: 500,
   })
-  .https.onRequest(async (req: any, res: any) => /* .pubsub
-  .schedule('every Monday 00:00 AM')
-  .onRun(async (req: any, res: any)  */ {
+  .pubsub.schedule('every 10 minutes')
+  .onRun(async (req: any, res: any) => {
     const playlistUris: any = {};
     // instantiate an empty uri array per radio
     nova.map((radio) => (playlistUris[radio.playlistId] = []));
@@ -49,7 +87,7 @@ export const saveAndDeleteNovaEveryWeek = functions
         headers: {
           'Content-Type': 'application/json',
         },
-        url: 'http://localhost:5001/nova-jukebox/us-central1/getPlaylistTracks',
+        url: 'https://us-central1-nova-jukebox.cloudfunctions.net/getPlaylistTracks',
         data: {
           playlistId: radio.playlistId,
           novaChannel: radio.novaChannel,
@@ -84,7 +122,7 @@ export const saveAndDeleteNovaEveryWeek = functions
             headers: {
               'Content-Type': 'application/json',
             },
-            url: 'http://localhost:5001/nova-jukebox/us-central1/deleteTracks',
+            url: 'https://us-central1-nova-jukebox.cloudfunctions.net/deleteTracks',
             data: {
               playlistId: radio.playlistId,
               uris: playlistUris[radio.playlistId],
@@ -114,11 +152,159 @@ export const saveAndDeleteNovaEveryWeek = functions
           );
       })
       .catch((err) => console.log('something went wrong.. ', err));
-
-    res.json({
-      result: `Tracks saved and deleted.`,
-    });
   });
+
+/////////////////////// SCRAPE NOVA ///////////////////////
+async function scrapeNovaTrackIds(radioValue: string): Promise<string[]> {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  // mock CET time
+  await page.emulateTimezone('Europe/Brussels');
+
+  // go to Nova
+  await page.goto('https://www.nova.fr/c-etait-quoi-ce-titre/');
+  // accept cookies
+  await page.click('[id="didomi-notice-agree-button"]');
+  // select nouvo nova radio
+  await page.select('#radio', radioValue);
+
+  // save the form
+  await page.evaluate(() => {
+    const element: HTMLElement | null = document.querySelector(
+      '#js-mobile-program-filter > a'
+    );
+    element?.click();
+  });
+
+  // wait for page to load
+  await page.waitForTimeout(2000);
+
+  // get spotify links
+  const data = await page.evaluate(async () => {
+    const links = document.querySelectorAll('a');
+    const urls = Array.from(links)
+      .map((link) => link.href)
+      .filter((href) => href.includes('spotify'));
+
+    return urls;
+  });
+
+  await browser.close();
+
+  // save trackIds
+  const trackIds: string[] = [];
+  data.map((trackUrl: string) => {
+    const trackId = trackUrl.substring(trackUrl.indexOf('k') + 2);
+    trackIds.push(trackId);
+  });
+
+  return trackIds;
+}
+
+////////////////// SAVE NOVA SONGS ON SPOTIFY //////////////////
+exports.saveNovaOnSpotify = functions
+  .runWith({
+    timeoutSeconds: 500,
+    memory: '512MB',
+  })
+  .https.onRequest(async (req: any, res: any) => {
+    const trackIds = await scrapeNovaTrackIds(req.body.frequence);
+
+    const headers = await getSpotifyAuthHeaders();
+
+    const playlistLastTrackIds = await getPlaylistLastTrackIds(
+      headers,
+      req.body.playlistId,
+      limitTracksToCheck
+    );
+
+    // filter nova trackIds with duplicates
+    const filteredIds = trackIds.filter(
+      (id) => !playlistLastTrackIds.includes(id)
+    );
+
+    // format uris
+    const uris = filteredIds.map((id) => `spotify:track:${id}`).reverse();
+
+    const response = await saveTracksToPlaylist(
+      headers,
+      req.body.playlistId,
+      uris
+    );
+
+    res.end(response);
+  });
+
+/////////////////////// GET PLAYLIST LAST TRACK IDS ///////////////////////
+async function getPlaylistLastTrackIds(
+  headers: Object,
+  playlistId: string,
+  limit: number
+): Promise<string[]> {
+  // get nova playlist total track number
+  let novaPlaylistTotal = 0;
+  await axios({
+    headers,
+    url: `https://api.spotify.com/v1/playlists/${playlistId}`,
+  }).then((response: any) => {
+    novaPlaylistTotal = response.data.tracks.total;
+  });
+
+  // get nova playlist last track ids:
+  // variables
+  const offset = novaPlaylistTotal - limit > 0 ? novaPlaylistTotal - limit : 0;
+  const lastTracks: any[] = [];
+
+  // request
+  await axios({
+    headers,
+    url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}&offset=${offset}`,
+  }).then(
+    (response: any) => {
+      lastTracks.push(response.data.items);
+    },
+    (error: any) => {
+      console.log('error: ', error);
+    }
+  );
+
+  // formatting
+  const lastTrackIds = lastTracks.map((track) => {
+    return track.map((t: any) => t.track.id);
+  })[0];
+
+  return lastTrackIds;
+}
+
+/////////////////////// SAVE TRACKS TO SPOTIFY ///////////////////////
+async function saveTracksToPlaylist(
+  headers: Object,
+  playlistId: string,
+  uris: string[]
+): Promise<string> {
+  let res = '';
+  // add tracks to playlist
+  if (uris.length > 0) {
+    await axios({
+      method: 'POST',
+      headers,
+      url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      data: {
+        uris: uris,
+      },
+    }).then(
+      (response: any) => {
+        console.log('response status: ', response.status);
+        res = `response status: ${response.status}`;
+      },
+      (error: any) => {
+        console.log('error: ', error);
+        res = `error: ${error}`;
+      }
+    );
+  }
+  return res;
+}
 
 /////////////////////// HEADERS FOR SPOTIFY API ///////////////////////
 // refresh access token
@@ -403,7 +589,7 @@ exports.getSpotifyToken = functions
       params.append('grant_type', 'authorization_code');
       params.append('code', data.code);
       // this redirect_uri must be exactly the same as used in environment.(prod).ts
-      params.append('redirect_uri', 'http://localhost:4200');
+      params.append('redirect_uri', 'https://nova-jukebox.web.app/');
     } else {
       params.append('grant_type', 'refresh_token');
       params.append('refresh_token', data.refreshToken);
