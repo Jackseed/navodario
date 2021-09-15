d2ea5767414795a377952acf18d15d841555ebb3
Jackseed <vinkho.c@gmail.com>
2021-09-15 20:07:32 +0200

    auth fix

--- src/app/spotify/spotify.service.ts
+++ src/app/spotify/spotify.service.ts
@@ -34,10 +34,7 @@ export class SpotifyService {
     const player = new Player({
       name: 'Nova Jukebox',
       getOAuthToken: async (callback: any) => {
-        let token = user.tokens.access;
-        const isTokenValid = await this.isTokenStillValid();
-
-        if (!isTokenValid) token = (await this.getToken()).token;
+        const token = (await this.getToken()).token;
 
         callback(token);
       },
