b974af94cc0fc82a4f28e1b9eb292d2bf7f5588e
Jackseed <vinkho.c@gmail.com>
2021-09-19 09:50:25 +0200

    renaming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('navodario app is running!');
+    expect(page.getTitleText()).toEqual('nova-jukebox app is running!');
   });
 
   afterEach(async () => {
